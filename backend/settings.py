d2ca9e5e89b17c48f2367a0f43324bb64a50d1f4
hosseincollege <hosseincollege@gmail.com>
2025-09-11 19:11:07 +0330

    Clean import statements and update STATIC_ROOT

--- backend/settings.py
+++ backend/settings.py
@@ -143,8 +143,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
 STATIC_URL = 'static/'
-import os
-
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 # Default primary key field type
